f2bde1f9372b5523855e5a268349506789afe4e3
dkrzyzak <damntheguitars@gmail.com>
2024-09-28 16:50:54 +0200

    add kalkulator page with dialog

---
+++ frontend/app/kalkulator/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog';
+
+export const CalculatorPage = () => {
+  return (
+    <>
+      <div>Kalkulator</div>
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default CalculatorPage;
