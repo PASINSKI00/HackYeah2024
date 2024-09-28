234ab66cc008e99f332630867a48617ba16557b6
Bartek <smalecbartlomiej@gmail.com>
2024-09-28 17:53:26 +0200

    Add additional fields to plant

--- Backend/src/main/java/org/example/trees/entities/Plant.java
+++ Backend/src/main/java/org/example/trees/entities/Plant.java
@@ -1,5 +1,6 @@
 package org.example.trees.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -8,10 +9,15 @@ public class Plant {
     private String desc;
     private String assimilated;
     private int id;
+    @JsonProperty("name_latin")
     private String nameLatin;
     private String name;
+    @JsonProperty("co_absorbed")
     private String coAbsorbed;
+    @JsonProperty("pm_removed")
     private String pmRemoved;
+    @JsonProperty("energy_saved")
     private String energySaved;
+    @JsonProperty("number_of_trees")
     private int numberOfTrees;
 }
