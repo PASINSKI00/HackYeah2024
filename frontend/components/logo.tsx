d3ce790e31967ef83dda5a6530748260325c7fda
dkrzyzak <damntheguitars@gmail.com>
2024-09-29 09:44:19 +0200

    add actual logo

--- frontend/components/logo.tsx
+++ frontend/components/logo.tsx
@@ -1,8 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 // CO2UNTER logo at the top left of the page
 export default function Logo() {
   return (
     <div className='ml-10 mt-10 flex items-center'>
-      <h1 className='text-xs font-bold'>CO2UNTER</h1>
+      <Link href='/'>
+        <Image
+          src='/logo.png'
+          width={300}
+          height={60}
+          alt='logo'
+          className='h-auto w-32'
+        />
+      </Link>
     </div>
   );
 }
