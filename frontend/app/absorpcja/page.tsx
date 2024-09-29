fb9b168b33bab9388e47259d41777f2bd86be8ad
Bartek <smalecbartlomiej@gmail.com>
2024-09-29 02:59:16 +0200

    Fix eslint

--- frontend/app/absorpcja/page.tsx
+++ frontend/app/absorpcja/page.tsx
@@ -2,11 +2,16 @@
 
 import Section from '@/components/ui/section';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import {countTree} from '@/app/api';
-import {useEffect, useState} from 'react';
+import { countTree } from '@/app/api';
+import { useEffect, useState } from 'react';
+
+interface TreeCount {
+    young_tree: number;
+    '100_old_tree': number;
+}
 
 export default function Page() {
-    const [treeCount, setTreeCount] = useState<number | null>(null);
+    const [treeCount, setTreeCount] = useState<TreeCount | null>(null);
     const [data, setData] = useState(350);
     const [daily, setDaily] = useState(350);
 
@@ -16,14 +21,13 @@ export default function Page() {
         }
     }, []);
 
-
     useEffect(() => {
         if (data) {
             const fetchTreeCount = async () => {
                 console.log(Number(localStorage.getItem('totalValue')) / 1000);
                 try {
                     const count = await countTree(Number(localStorage.getItem('totalValue')) / 1000); // Example sum value
-                    let number = Number(localStorage.getItem('totalValue')) / Math.round((count?.['100_old_tree'] || 1));
+                    const number = Number(localStorage.getItem('totalValue')) / Math.round((count?.['100_old_tree'] || 1));
                     setDaily(Math.round(number));
                     setTreeCount(count);
                 } catch (error) {
@@ -34,23 +38,15 @@ export default function Page() {
             fetchTreeCount();
             console.log(treeCount);
         }
-    }, [data]);
-
-    useEffect(() => {
-        if (data) {
-
-        }
-    }, []);
-
+    }, [data, treeCount]);
 
     return (
-
         <main className='mb-12 mt-4 grid gap-12 px-6'>
             <Section
                 color='green'
                 label='Zieleń miejska'
                 value={`${data}g`}
-                description={`${data} CO2 zostanie pochłonięte po ${Math.round(treeCount?.['young_tree'] || 0)} dniach przez małe sadzonki lub ${Math.round(treeCount?.['100_old_tree'] || 0)} dni przez 100-letnie drzewo.`}
+                description={`${data} CO2 zostanie pochłonięte po ${Math.round(treeCount?.young_tree || 0)} dniach przez małe sadzonki lub ${Math.round(treeCount?.['100_old_tree'] || 0)} dni przez 100-letnie drzewo.`}
                 imageSrc='/outdoor.png'
                 statistics={[]}
             />
@@ -64,7 +60,7 @@ export default function Page() {
                 statistics={[
                     {
                         label: 'Dziennie',
-                        value: daily + " ",
+                        value: daily,
                         unit: 'co2',
                         Icon: CalendarTodayIcon
                     },
@@ -73,5 +69,3 @@ export default function Page() {
         </main>
     );
 }
-
-
